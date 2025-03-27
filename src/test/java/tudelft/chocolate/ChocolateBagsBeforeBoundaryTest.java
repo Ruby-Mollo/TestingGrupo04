1147a3a722163d977863610f12e1eaaa0d76bc4d
Ruby-Mollo <lopez.lrd07.com>
2025-03-27 18:19:07 -0500

    chore: Actividad 3: Barras de Chocolate, modificando los test

--- src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
@@ -7,25 +7,25 @@ public class ChocolateBagsBeforeBoundaryTest {
     @Test
     public void totalIsBiggerThanAmountOfBars() {
         ChocolateBags bags = new ChocolateBags();
-        int result = bags.calculate(1, 1, 10);
-        Assertions.assertEquals(-1, result);
-    }
-
-    @Test
-    public void onlyBigBars() {
-        int result = new ChocolateBags().calculate(5, 3, 10);
+        int result = bags.calculate(1, 1, 5);
         Assertions.assertEquals(0, result);
     }
 
     @Test
+    public void onlyBigBars() {
+        int result = new ChocolateBags().calculate(1, 1, 6);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
     public void bigAndSmallBars() {
-        int result = new ChocolateBags().calculate(5, 3, 17);
-        Assertions.assertEquals(2, result);
+        int result = new ChocolateBags().calculate(1, 1, 7);
+        Assertions.assertEquals(-1, result);
     }
 
     @Test
     public void onlySmallBars() {
-        int result = new ChocolateBags().calculate(4, 2, 3);
-        Assertions.assertEquals(3, result);
+        int result = new ChocolateBags().calculate(1, 1, 8);
+        Assertions.assertEquals(-1, result);
     }
 }
